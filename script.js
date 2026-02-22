6e23590741538827e091bcdf22bc47ad492f3782
Franta020 <oofranta@gmail.com>
2026-02-22 21:47:41 +0100

    css done, naming js constants

---
+++ script.js
@@ -0,0 +1,10 @@
+const balance = $("#cash");
+const income = $("#income");
+const expense = $("#expense");
+const trackContainer = $(".track-container-a");
+const addIncome = $("#positive-btn");
+const addExpense = $("#negative-btn");
+const positiveLayer = $(".transaction-positive");
+const negativeLayer = $("transaction-negative");
+
+let transItems = [];
